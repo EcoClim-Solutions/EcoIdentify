2e8d799ab9f0be67bdea0bac424439625f7caf94
EcoClim-Solutions <151465374+EcoClim-Solutions@users.noreply.github.com>
2024-01-01 22:34:49 +0000

    changed utils

--- utils.py
+++ utils.py
@@ -6,18 +6,18 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import numpy as np
 from PIL import Image
 import tensorflow as tf
-from Downloading_model import *
+from Downloading_model import model_download  # Assuming model_download is correctly implemented
 
 def gen_labels():
     train = 'Dataset/Train'
-    train_generator = ImageDataGenerator(rescale = 1/255)
+    train_generator = ImageDataGenerator(rescale=1/255)
 
     train_generator = train_generator.flow_from_directory(train,
-                                                        target_size = (300,300),
-                                                        batch_size = 32,
-                                                        class_mode = 'sparse')
-    labels = (train_generator.class_indices)
-    labels = dict((v,k) for k,v in labels.items())
+                                                        target_size=(300, 300),
+                                                        batch_size=32,
+                                                        class_mode='sparse')
+    labels = train_generator.class_indices
+    labels = dict((v, k) for k, v in labels.items())
 
     return labels
 
@@ -29,20 +29,20 @@ def preprocess(image):
     return image
 
 data_augmentation = tf.keras.Sequential([
-    tf.keras.layers.experimental.preprocessing.Rescaling(1./127.5, offset= -1),
+    tf.keras.layers.experimental.preprocessing.Rescaling(1./127.5, offset=-1),
     tf.keras.layers.experimental.preprocessing.RandomFlip("horizontal_and_vertical"),
     tf.keras.layers.experimental.preprocessing.RandomRotation(0.2),
     tf.keras.layers.experimental.preprocessing.RandomZoom(0.2)
 ], name='data_augmentation')
 
-#Instantiating the base model
-input_shape = (256,256,3)
+# Instantiating the base model
+input_shape = (256, 256, 3)
 base_model = tf.keras.applications.ResNet50V2(include_top=False, input_shape=input_shape)
 
-#Making the layers of the model trainable
+# Making the layers of the model trainable
 base_model.trainable = True
 
 def model_arc():
-    model_download()
-    model = tf.keras.models.load_model(model_download())
+    model_path = model_download()
+    model = tf.keras.models.load_model(model_path)
     return model
