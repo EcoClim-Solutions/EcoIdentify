35b39cf6de02a6bb49f077d8407d63aaaa4d22df
EcoClim-Solutions <151465374+EcoClim-Solutions@users.noreply.github.com>
2024-01-01 22:32:22 +0000

    changed drive file

--- Downloading_model.py
+++ Downloading_model.py
@@ -1,7 +1,7 @@
 import gdown
 
 def model_download():
-    urlforinstall = 'https://drive.google.com/file/d/19cHovYhc-fiTCtQ2aXNlB5mozwTdKscx/view?usp=drive_link'
+    urlforinstall = 'https://drive.google.com/file/d/19cHovYhc-fiTCtQ2aXNlB5mozwTdKscx/view?usp=sharing'
     outputforinstall = 'EcoIdentify_modellink.h5'
 
     gdown.download(urlforinstall, outputforinstall, quiet=False)
