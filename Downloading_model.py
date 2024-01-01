48a2c6888149cb9845f30ae3b3288115825771ff
VishnuEcoClim <154030618+VishnuEcoClim@users.noreply.github.com>
2024-01-01 16:11:43 -0700

    Update Downloading_model.py

--- Downloading_model.py
+++ Downloading_model.py
@@ -2,7 +2,7 @@ import gdown
 
 def model_download():
     urlforinstall = 'https://www.dropbox.com/scl/fi/8lxjfo0ebfd7kgb0sito6/EcoIdentify_official_classification_model.h5?rlkey=35jdpwthtr4fbfehz02abozf5&dl=1'
-    outputforinstall = 'EcoIdentify_modellink.h5'
+    outputforinstall = 'EcoIdentify_official_classification_model.h5'
 
     gdown.download(urlforinstall, outputforinstall, quiet=False)
 
