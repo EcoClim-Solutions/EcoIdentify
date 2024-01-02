64a4b7e6ddc5622e536a27f62276dc2f75d2c632
EcoClim-Solutions <151465374+EcoClim-Solutions@users.noreply.github.com>
2024-01-02 00:54:14 +0000

    test

--- utils.py
+++ utils.py
@@ -1,48 +1,39 @@
-import gdown
-import tensorflow as tf
-from PIL import Image
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, Flatten, MaxPooling2D, Dense, Dropout, SpatialDropout2D
+from tensorflow.keras.losses import sparse_categorical_crossentropy, binary_crossentropy
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import numpy as np
-import Downloading_model
+from PIL import Image
+import tensorflow as tf
+from Downloading_model import model_download  # Assuming model_download is correctly implemented
 
-model = tf.keras.models.load_model(Downloading_model.main)
+def gen_labels():
+    train = 'Dataset/Train'
+    train_generator = ImageDataGenerator(rescale=1/255)
 
-labels = ['cardboard', 'glass', 'metal', 'paper', 'plastic', 'trash']
+    train_generator = train_generator.flow_from_directory(train,
+                                                        target_size=(300, 300),
+                                                        batch_size=32,
+                                                        class_mode='sparse')
+    labels = train_generator.class_indices
+    labels = dict((v, k) for k, v in labels.items())
 
-def model_download():
-    urlforinstall = 'https://www.dropbox.com/scl/fi/8lxjfo0ebfd7kgb0sito6/EcoIdentify_official_classification_model.h5?rlkey=35jdpwthtr4fbfehz02abozf5&dl=1'
-    outputforinstall = 'EcoIdentify_official_classification_model.h5'
-
-    gdown.download(urlforinstall, outputforinstall, quiet=False)
-
-    # Return the path where the model is saved
-    return outputforinstall
-
+    return labels
 
 def preprocess(image):
     # Resize the image
     image = image.resize((256, 256), Image.LANCZOS)
-
+    
     # Convert the image to a NumPy array and scale its values to [0, 1]
     image_array = np.array(image, dtype=np.float32) / 255.0
-
+    
     # Add an extra dimension for batch size
     image_array = np.expand_dims(image_array, axis=0)
-
+    
     return image_array
 
-
-def predict(image):
-    # Preprocess the image
-    image_array = preprocess(image)
-
-    # Make a prediction
-    prediction = model.predict(image_array)
-
-    # Get the index of the class with the highest probability
-    top_class_idx = np.argmax(prediction)
-
-    # Get the class name and confidence
-    top_class = labels[top_class_idx]
-    confidence = prediction[0][top_class_idx]
-
-    return top_class, confidence
+def model_arc():
+    model_path = model_download()
+    model = tf.keras.models.load_model(model_path)
+    return model
