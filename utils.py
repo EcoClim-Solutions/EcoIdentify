ff13681c5f46a8c3469dbad99635cf4a9414fbac
VishnuEcoClim <154030618+VishnuEcoClim@users.noreply.github.com>
2024-01-01 21:52:39 -0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -77,22 +77,22 @@ def get_default_device():
 device
 
 def to_device(data, device):
-    """Move tensor(s) to chosen device"""
+    Move tensor(s) to chosen device
     if isinstance(data, (list,tuple)):
         return [to_device(x, device) for x in data]
     return data.to(device, non_blocking=True)
 
 class DeviceDataLoader():
-    #"""Wrap a dataloader to move data to a device"""
+    Wrap a dataloader to move data to a device
     def __init__(self, dl, device):
         self.dl = dl
         self.device = device
         
     def __iter__(self):
-      #  """Yield a batch of data after moving it to device"""
+      Yield a batch of data after moving it to device
         for b in self.dl: 
             yield to_device(b, self.device)
 
     def __len__(self):
-       # """Number of batches"""
+      Number of batches
         return len(self.dl)"""
