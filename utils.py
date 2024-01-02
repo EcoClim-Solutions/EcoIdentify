3f88f4a475bb02893f4facbfc34b5cda2c17c408
EcoClim-Solutions <151465374+EcoClim-Solutions@users.noreply.github.com>
2024-01-02 20:01:34 +0000

    ch

--- utils.py
+++ utils.py
@@ -33,34 +33,8 @@ def predict_image(img, model, labels):
     # Pick index with the highest probability
     _, preds = torch.max(probabilities, dim=1)
     
-<<<<<<< HEAD
     # Retrieve the class label, confidence, and probabilities
     class_label = labels[preds[0].item()]
     confidence = probabilities[0, preds[0].item()].item()
     
     return class_label, confidence, probabilities.tolist()[0]
-=======
-"""device = get_default_device()
-device
-
-def to_device(data, device):
-    Move tensor(s) to chosen device
-    if isinstance(data, (list,tuple)):
-        return [to_device(x, device) for x in data]
-    return data.to(device, non_blocking=True)
-
-class DeviceDataLoader():
-    Wrap a dataloader to move data to a device
-    def __init__(self, dl, device):
-        self.dl = dl
-        self.device = device
-        
-    def __iter__(self):
-      Yield a batch of data after moving it to device
-        for b in self.dl: 
-            yield to_device(b, self.device)
-
-    def __len__(self):
-      Number of batches
-        return len(self.dl)"""
->>>>>>> refs/remotes/origin/main
