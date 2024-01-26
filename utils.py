281c10ea1e561258ac88499f91d877681c3391e7
Vishnu-Raja-Kumar <153052214+Vishnu-Raja-Kumar@users.noreply.github.com>
2024-01-25 20:55:34 -0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,16 +1,12 @@
 from PIL import Image
 import numpy as np
-from Downloading_model import model_download
 
 labels = ['cardboard', 'glass', 'metal', 'paper', 'plastic', 'trash']
-model = model_download("https://onedrive.live.com/download?resid=657A29EC827C9C58%21107&authkey=!APDOTvOiL9qk5wc")
-
 
 def preprocess_image(img_path):
     img = Image.open(img_path)
     img = img.resize((256, 256))
     img_array = np.array(img)
-    img_array = np.expand_dims(img_array, axis=0)
     return img_array
 
 # Function to classify the garbage
